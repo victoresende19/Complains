93fbc1b8d71631a0a116f4d5adb7868bf2de47a1
Victor Resende <63743020+victoresende19@users.noreply.github.com>
2024-07-29 22:11:31 -0300

    Update topic.py

--- utils/topic.py
+++ utils/topic.py
@@ -11,11 +11,7 @@ import streamlit as st
 
 @st.cache_data(show_spinner=False, ttl=24*3600, max_entries=5)
 def downloads_nlp():
-    try:
-        nlp = spacy.load('pt_core_news_sm')
-    except OSError:
-        download("pt_core_news_sm")
-        nlp = spacy.load('pt_core_news_sm')
+    nlp = spacy.load('pt_core_news_sm')
     return nlp
 
 def normalize(df: pd.DataFrame, column: str):
